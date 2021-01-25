8460a957bbd1a6df3100f13b913ee5f654217092
Raimedas Simutis <raimedas.simutis@gmail.com>
2021-01-25 21:33:45 +0200

    added methods to get and store weather data

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -36,6 +36,9 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  created () {
+    this.$store.dispatch('fetchWeather', 'Vilnius')
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    weatherData: null,
+    currentCity: null
   },
   mutations: {
+    SET_WEATHER (state, payload) {
+      state.weatherData = payload
+    }
   },
   actions: {
+    fetchWeather ({ commit, state }, city) {
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.VUE_APP_WEATHER_API_KEY}`)
+        .then(response => {
+          commit('SET_WEATHER', response)
+        })
+    }
   },
   modules: {
   }
