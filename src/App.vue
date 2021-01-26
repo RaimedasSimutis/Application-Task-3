b184828bdc508066147f029c086a669cdc628359
Raimedas Simutis <raimedas.simutis@gmail.com>
2021-01-26 22:23:56 +0200

    base view layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Index msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <Index/>
   </div>
 </template>
 
@@ -17,12 +17,15 @@ export default {
 </script>
 
 <style lang="scss">
+  @import "styles/normalize.css";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+  width: 100vw;
+  height: 100vh;
 }
 </style>
---
+++ src/components/WeekForecast.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="forecast-week">
+    <div v-if="title" class="forecast-week__heading">title</div>
+    <div class="forecast-week__content">
+      <div
+        v-for="(day, index) in weatherData"
+        :key="index"
+      >
+        <div class="forecast-week__day-title">
+          {{ day.day }}
+        </div>
+
+        <div class="forecast-week__icon">
+          {{ day.icon }}
+        </div>
+
+        <div class="forecast-week__stats">
+          <div class="forecast-week__stat">
+            {{day.temperatureDay}}
+          </div>
+          <div class="forecast-week__stat">
+            {{day.temperatureNight}}
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WeekForecast',
+  props: {
+    weatherData: {
+      type: Array,
+      required: true
+    },
+    title: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .forecast-week {
+
+    &__heading {
+      height: 35px;
+      background: #3A424C;
+    }
+
+    &__content {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &__stats {
+      display: flex;
+      justify-content: space-around;
+    }
+  }
+</style>
