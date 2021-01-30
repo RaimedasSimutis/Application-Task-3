5555b353eead45e3534931a1c86e9ab1a30b977d
Raimedas Simutis <raimedas.simutis@gmail.com>
2021-01-31 00:00:08 +0200

    search component and its styling fixes

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,23 +1,29 @@
 <template>
-  <section class="search" v-show="isActive">
-    <navigation-bar
-      :leftButtonData="leftNavButtonData"
-      @leftButtonClick="closeSearch"
-    />
-    <div>
-      <input v-model="inputValue" :placeholder="placeholder">
-    </div>
-    <div>
-      <div v-if="results">
-        <div v-for="(result, index) in results" :key="index" @click="closeSearch(result)">
-          {{result.name}}
+  <transition name="fade">
+    <section class="search" v-show="isActive">
+      <navigation-bar
+        :leftButtonData="leftNavButtonData"
+        @leftButtonClick="closeSearch"
+      />
+      <div class="search__input-container">
+        <input class="search__input" v-model="inputValue" :placeholder="placeholder">
+        <div class="search__icon-container">
+          <i class="fas fa-search search__input-icon"></i>
+
         </div>
       </div>
-      <div v-else>
-        {{noDataText}}
+      <div class="search__results-container">
+        <div v-if="results">
+          <div v-for="(result, index) in results" :key="index" @click="closeSearch(result)">
+            {{result.name}}
+          </div>
+        </div>
+        <div class="search__no-results" v-else>
+          {{noDataText}}
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -71,7 +77,7 @@ export default {
           }
         }
 
-        return filterResults
+        return filterResults.length ? filterResults : null
       }
       return null
     }
@@ -97,6 +103,55 @@ export default {
     width: 100vw;
     top: 0;
     right: 0;
-    background: rgba(0, 255, 255, 0.68);
+    background-color: #4E555E;
+
+    &__input-container {
+      padding: 7px;
+      position: relative;
+      background-color: #3A424C;
+    }
+
+    &__input {
+      width: 100%;
+      border-radius: 5px;
+      border: 1px solid #4C9FC7;
+      background-color: #484F58;
+      color: #FFFFFF;
+      font-size: 10px;
+      line-height: 20px;
+      padding: 0 24px;
+
+      &::placeholder {
+        color: #9FA0A5;
+      }
+    }
+
+    &__icon-container{
+      position: absolute;
+      top: 7px;
+      left: 14px;
+      height: 22px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+    }
+
+    &__input-icon {
+      font-size: 10px;
+      color: #9E9FA4;
+    }
+
+    &__results-container {
+      height: calc(100% - 61px);
+    }
+
+    &__no-results {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
+
 </style>
