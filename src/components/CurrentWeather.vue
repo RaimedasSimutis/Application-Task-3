cac7cb68bf1663904cf3987a473eda840e8e59bc
Raimedas Simutis <raimedas.simutis@gmail.com>
2021-01-30 23:58:50 +0200

    openweather icons added

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,13 +1,18 @@
 <template>
   <section class="current-weather">
     <div class="current-weather__temperature">
-      {{temperature}}
+      {{convertedTemperature}}<span v-html="temperatureSign"></span>
     </div>
     <div class="current-weather__city">
       {{location}}
     </div>
     <div class="current-weather__weather">
-      {{description}} {{icon}}
+      <div>
+        {{description}}
+      </div>
+      <div>
+        <img :src="`http://openweathermap.org/img/wn/${icon}.png`" alt="weather-icon"/>
+      </div>
     </div>
   </section>
 </template>
@@ -17,9 +22,24 @@ export default {
   name: 'CurrentWeather',
   props: {
     location: String,
-    temperature: String,
+    temperature: Number,
     icon: String,
-    description: String
+    description: String,
+    temperatureUnit: {
+      type: String
+    }
+  },
+  computed: {
+    convertedTemperature () {
+      return this.temperature ? Math.round(this.temperature - 273.15) : '--'
+    },
+    temperatureSign () {
+      switch (this.temperatureUnit) {
+        case 'C': return '&#176;'
+        case 'F': return '&#176;'
+      }
+      return '&#176;'
+    }
   }
 }
 </script>
@@ -33,6 +53,12 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    &__weather {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
 
 </style>
